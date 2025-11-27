4cbdb197680fab634d71fb9ddd2a730005d0b027
auwaldev-creator <auwalbashar12@gmail.com>
2025-11-27 22:59:54 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,19 @@
-// Wannan file din JavaScript ne. Zamu barshi fanko a yanzu,
-// amma za'a iya amfani dashi don ƙara wani abu mai motsi, misali kirga kwanaki.
-
-console.log("Welcome to Haruna & Hassana Wedding Fatiha Page!");
+document.getElementById('download-btn').addEventListener('click', function() {
+    const cardElement = document.getElementById('download-card');
+    
+    // An amfani da html2canvas don mayar da HTML zuwa Hoto
+    html2canvas(cardElement, { scale: 2 }).then(function(canvas) {
+        // Mayar da canvas zuwa Data URL (PNG format)
+        const image = canvas.toDataURL("image/png");
+        
+        // Ƙirƙirar wani <a> element don download
+        const link = document.createElement('a');
+        link.href = image;
+        link.download = 'Haruna_Hassana_Wedding_Card.png';
+        
+        // Danna link ɗin a ɓoye don fara downloading
+        document.body.appendChild(link);
+        link.click();
+        document.body.removeChild(link);
+    });
+});
