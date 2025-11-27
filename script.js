14f5c0d5b6ef1e592d164ada3418fd8d92377fdf
auwaldev-creator <auwalbashar12@gmail.com>
2025-11-27 22:24:28 +0100

    Create script.js

---
+++ script.js
@@ -0,0 +1,4 @@
+// Wannan file din JavaScript ne. Zamu barshi fanko a yanzu,
+// amma za'a iya amfani dashi don ƙara wani abu mai motsi, misali kirga kwanaki.
+
+console.log("Welcome to Haruna & Hassana Wedding Fatiha Page!");
